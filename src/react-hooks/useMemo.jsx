d749601ceb58fce90c3920fc17eba1e3901162ec
wudingxuan9639 <584319437@qq.com>
2022-03-09 10:54:51 +0800

    memo fix

--- src/react-hooks/useMemo.jsx
+++ src/react-hooks/useMemo.jsx
@@ -1,42 +1,37 @@
-import React, { useState } from "react";
-// import ReactDOM from "react-dom";
+import React, { useCallback, useMemo, useState } from "react";
+
 
 function App() {
-    const [n, setN] = useState(0);
-    const [m, setM] = useState(10);
-    console.log("执行最外层盒子了");
-    return (
-      <>
-        <div>
-          最外层盒子
-          <Child1 value={n} />
-          <Child2 value={m} />
-          <button
-            onClick={() => {
-              setN(n + 1);
-            }}
-          >
-            n+1
-          </button>
-          <button
-            onClick={() => {
-              setM(m + 1);
-            }}
-          >
-            m+1
-          </button>
-        </div>
-      </>
-    );
-  }
-  const Child1 = React.memo((props) => {
-    console.log("执行子组件1了");
-    return <div>子组件1上的n：{props.value}</div>;
-  });
-  
-  const Child2 = React.memo((props) => {
-    console.log("执行子组件2了");
-    return <div>子组件2上的m：{props.value}</div>;
-  });
+  const [n, setN] = useState(0);
+  const [m, setM] = useState({ m: 1 });
+  console.log("执行最外层盒子了");
+  const addN = useMemo(() => {
+    return () => {
+      setN(n + 1);
+    };
+  }, [n]);
+  const addM = useCallback(() => {
+    setM({ m: m.m + 1 });
+  }, [m]);
+  return (
+    <>
+      <div>
+        最外层盒子
+        <Child1 value={n} click={addN} />
+        <Child2 value={m} click={addM} />
+        <button onClick={addN}>n+1</button>
+        <button onClick={addM}>m+1</button>
+      </div>
+    </>
+  );
+}
+const Child1 = React.memo((props) => {
+  console.log("执行子组件1了");
+  return <div>子组件1上的n：{props.value}</div>;
+});
 
+const Child2 = React.memo((props) => {
+  console.log("执行子组件2了");
+  return <div>子组件2上的m：{props.value.m}</div>;
+});
   export default App;
